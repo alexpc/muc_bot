dfcd4b2064a490bd16bf73bde6c8efc4e03353ff
Aleksander Honc <alexpc@gmail.com>
2012-04-18 12:31:55 +0200

    multi-threading, as example sends timestamp to MUC every minute

--- muc_bot.rb
+++ muc_bot.rb
@@ -16,86 +16,116 @@ def print_line(time, line)
   if time.nil?
     puts line
   else
-    puts "#{time.strftime('%I:%M')} #{line}"
+    puts "#{time.strftime('%H:%M')} #{line}"
   end
+end
+
+class Connector
+    include Singleton
+    def initialize
+        @cl = Jabber::Client.new(Jabber::JID.new(ARGV[0]))
+        @cl.connect
+        @cl.auth(ARGV[1])
+        @m = Jabber::MUC::SimpleMUCClient.new(@cl)
+        @m.join(ARGV[2])
+    end
+    def muc
+        return @m
+    end
+end
+
+class Logger
+    include Singleton
+    def initialize
+        @buffer = ''
+    end
+    def log(msg)
+        @buffer += msg + "\n"
+        if @buffer.length > 1
+            filename = Time.new.strftime('%Y-%m-%d_log.txt')
+            aFile = File.new(filename, "a")
+            aFile.write(@buffer)
+            aFile.close
+            @buffer = ''
+        end
+    end
 end
 
 
 begin
 #Jabber::debug = true
-buffor = ''
-cl = Jabber::Client.new(Jabber::JID.new(ARGV[0]))
-cl.connect
-cl.auth(ARGV[1])
+#cl = Jabber::Client.new(Jabber::JID.new(ARGV[0]))
+#cl.connect
+#cl.auth(ARGV[1])
 
 
 # This is the SimpleMUCClient helper!
-m = Jabber::MUC::SimpleMUCClient.new(cl)
-
-
+m = Connector.instance.muc
+logger = Logger.instance
 # For waking up...
 mainthread = Thread.current
 
+eventrunner = Thread.new{
+	sleep(0.1) while mainthread.status!='sleep'
+	begin
+		m.say( "Mamy godzinę " + Time.new.strftime("%H:%M:%S"))
+		sleep(60)
+	end while 1==1
+}
+
+
+
 # SimpleMUCClient callback-blocks
 m.on_join { |time,nick|
-  print_line time, "#{nick} has joined!"
-  puts "Users: " + m.roster.keys.join(', ')
-  buffor += "@@ #{nick} dołączył do pokoju!\n"
+	print_line time, "#{nick} has joined!"
+	puts "Users: " + m.roster.keys.join(', ')
+	logger.log("@@ #{nick} dołączył do pokoju!")
 }
 m.on_leave { |time,nick|
-  print_line time, "#{nick} has left!"
-  buffor += "@@ #{nick} wyszedł z pokoju!"
+	print_line time, "#{nick} has left!"
+	logger.log("@@ #{nick} wyszedł z pokoju!")
 }
 m.on_private_message{ |time,nick,text|
 	print_line time, "<#{nick}> #{text}"
-	#m.say(text,nick)
 	if text.strip =~ /^rzu[cćt] (\d*)k(\d+)/i
 		l_kosci = $1.to_i > 1 ? $1.to_i : 1
 		w_kosci = $2.to_i > 1 ? $2.to_i : 6
+		sleep(0.5)
 		m.say("Rzut #{l_kosci}k#{w_kosci}: #{l_kosci*(rand(w_kosci)+1)}",nick) 
 	end
 }
-
 m.on_message { |time,nick,text|
-  print_line time, "<#{nick}> #{text}"
-
-  # Avoid reacting on messaged delivered as room history
-  unless time
-    # Bot: invite astro@spaceboyz.net
-    if text.strip =~ /^(.+?): invite (.+)$/
-      jid = $2
-      if $1.downcase == m.jid.resource.downcase
-        m.invite(jid => "Inviting you on behalf of #{nick}")
-        m.say("Inviting #{jid}...")
-      end
-    # Bot: subject This is room is powered by XMPP4R
-    elsif text.strip =~ /^(.+?): subject (.+)$/
-      if $1.downcase == m.jid.resource.downcase
-        m.subject = $2
-      end
-    # Bot: exit please
-    elsif text.strip =~ /^(.+?): exit please$/
-      if $1.downcase == m.jid.resource.downcase
-        puts "exiting"
-        m.exit "Exiting on behalf of #{nick}"
-        mainthread.wakeup
-      end
-    else
-  		if text.include? '\me'
-			buffor += "[#{Time.new.strftime('%I:%M')}] #{text.gsub("\me",nick)}\n"
-	    else
-	  		buffor += "[#{Time.new.strftime('%I:%M')}] <#{nick}>: #{text}\n"
-	    end
+	print_line time, "<#{nick}> #{text}"
+	# Avoid reacting on messaged delivered as room history
+	unless time
+		# Bot: invite astro@spaceboyz.net
+		if text.strip =~ /^(.+?): invite (.+)$/
+			jid = $2
+			if $1.downcase == m.jid.resource.downcase
+				m.invite(jid => "Inviting you on behalf of #{nick}")
+				m.say("Inviting #{jid}...")
+			end
+		# Bot: subject This is room is powered by XMPP4R
+		elsif text.strip =~ /^(.+?): subject (.+)$/
+			if $1.downcase == m.jid.resource.downcase
+				m.subject = $2
+				logger.log("** #{m.subject}")
+			end
+		# Bot: exit please
+		elsif text.strip =~ /^(.+?): exit please$/
+			if $1.downcase == m.jid.resource.downcase
+				puts "exiting"
+				m.exit "Exiting on behalf of #{nick}"
+				mainthread.wakeup
+			end
+		else
+			if text.include? '\me'
+				logger.log("[#{Time.new.strftime('%I:%M')}] #{text.gsub("\me",nick)}")
+		    else
+				logger.log("[#{Time.new.strftime('%I:%M')}] <#{nick}>: #{text}")
+		    end
+		end
 	end
-  end
-
-  if buffor.length > 100
-	filename = Time.new.strftime('%Y-%m-%d_log.txt')
-	aFile = File.new(filename, "a")
-	aFile.write(buffor)
-	aFile.close
-	buffor = ''
-  end	
 }
 m.on_room_message { |time,text|
   print_line time, "- #{text}"
@@ -108,23 +138,9 @@ m.on_subject { |time,nick,subject|
 
 
 
-m.join(ARGV[2])
-
-
-=begin
-eventrunner = Thread.start{|m|
-	sleep(1) while mainthread.status!='sleep'
-	begin
-		m.say( "Aktywator!" + Time.new.strftime("%H:%I:%S"))
-		sleep(rand(5))
-	end while 1==1
-}
-eventrunner.run
-=end
-
-
 # Wait for being waken up by m.on_message
 Thread.stop
+
 
 
 cl.close
